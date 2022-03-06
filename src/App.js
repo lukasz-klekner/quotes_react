b4a59e89959788c4759f65b2af2baaab0ad2e96b
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-06 07:34:16 +0100

    adds redirecting and uses params

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import AllQuotes from './pages/AllQuotes'
 import NewQuote from './pages/NewQuote'
@@ -8,6 +8,9 @@ function App() {
   return (
     <div>
       <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
         <Route path='/quotes' exact>
           <AllQuotes />
         </Route>
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,11 @@
+import { useParams } from 'react-router-dom'
+
 const QuoteDetail = () => {
+  const { quoteId } = useParams(0)
   return (
     <>
       <h1>Quote Detail Page</h1>
+      {quoteId}
     </>
   )
 }
