34ffc65f23021feda3c7bd8fc21c41509831c763
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-06 11:27:49 +0100

    adds push

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 const NewQuote = () => {
+  const { push } = useHistory()
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData)
+
+    push('/quotes')
   }
 
   return <QuoteForm onAddQuote={addQuoteHandler} />
