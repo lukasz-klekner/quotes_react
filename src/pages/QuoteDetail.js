36eff22efe1553ceaf0521f0d0385a73cfce4bb6
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-06 07:47:28 +0100

    adds nested route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,17 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Route } from 'react-router-dom'
+
+import Comments from '../components/comments/Comments'
 
 const QuoteDetail = () => {
-  const { quoteId } = useParams(0)
+  const { quoteId } = useParams()
+
   return (
     <>
       <h1>Quote Detail Page</h1>
       {quoteId}
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
     </>
   )
 }
