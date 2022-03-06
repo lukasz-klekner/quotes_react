7a169be38ce843eed9f17779be7144af9d7e9be7
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-06 18:00:55 +0100

    posts quotes to firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,16 +1,25 @@
 import { useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
+
+import useHttp from '../hooks/useHttp'
+import { addQuote } from '../lib/api'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 const NewQuote = () => {
   const { push } = useHistory()
+  const { sendRequest, status } = useHttp(addQuote)
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData)
-
-    push('/quotes')
+    sendRequest(quoteData)
   }
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />
+  useEffect(() => {
+    if (status === 'completed') push('/quotes')
+  }, [status, push])
+
+  return (
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
+  )
 }
 
 export default NewQuote
