39895c4e3bb78a22583c01289d1328f0c54729b8
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-06 11:21:43 +0100

    adds content to QuoteDetail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,18 +1,30 @@
+import { Fragment } from 'react'
 import { useParams, Route } from 'react-router-dom'
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+]
 
 const QuoteDetail = () => {
   const { quoteId } = useParams()
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId)
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
+
   return (
-    <>
-      <h1>Quote Detail Page</h1>
-      {quoteId}
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
-    </>
+    </Fragment>
   )
 }
 
